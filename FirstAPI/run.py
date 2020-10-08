fe980b4a0f783a29c38bee2e9626428552ee15cc
AnastasiosMargaritis <anastasismargaritis@gmail.com>
2020-10-08 16:30:03 +0300

    Implementing Very Simple Token Based Auth

--- FirstAPI/run.py
+++ FirstAPI/run.py
@@ -1,8 +1,9 @@
 from eve import Eve
-from eve.auth import BasicAuth
+from eve.auth import BasicAuth, TokenAuth
 import bcrypt
 from db_testing import *
 
+#BCrypt Authentication 
 class BCryptAuth(BasicAuth):
     def check_auth(self, username, password, allowed_roles, resource, method):
         # use Eve's own db driver; no additional connections/resources are used
@@ -11,6 +12,12 @@ class BCryptAuth(BasicAuth):
         return account and bcrypt.hashpw(password, account['password']) == account['password']
 
 
+#Token Based Authentication
+class TokenAuth(TokenAuth):
+    def check_auth(self, token, allowed_roles, resource, method):
+        accounts = app.data.driver.db['user']
+        return accounts.find_one({'token': token})
+
 if __name__ == '__main__':
-    app = Eve(auth = BCryptAuth)
+    app = Eve(auth = TokenAuth)
     app.run()
