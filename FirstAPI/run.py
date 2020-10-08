0af46d746213a32f50b60350ee03c833c43ba8fc
AnastasiosMargaritis <anastasismargaritis@gmail.com>
2020-10-08 16:17:31 +0300

    Bug Fixing in BCrypt Auth

--- FirstAPI/run.py
+++ FirstAPI/run.py
@@ -6,9 +6,9 @@ from db_testing import *
 class BCryptAuth(BasicAuth):
     def check_auth(self, username, password, allowed_roles, resource, method):
         # use Eve's own db driver; no additional connections/resources are used
-        accounts = app.data.driver.db['accounts']
+        accounts = app.data.driver.db['user']
         account = accounts.find_one({'username': username})
-        return check_db_encryption(username, password)
+        return account and bcrypt.hashpw(password, account['password']) == account['password']
 
 
 if __name__ == '__main__':
