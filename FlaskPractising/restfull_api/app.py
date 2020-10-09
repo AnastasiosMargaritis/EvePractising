a66185f71aa3386bc3eec3218f411c471287f645
AnastasiosMargaritis <anastasismargaritis@gmail.com>
2020-10-09 16:02:44 +0300

    Adding advanced Parsing

--- FlaskPractising/restfull_api/app.py
+++ FlaskPractising/restfull_api/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required 
 
 from security import authenticate, identity
@@ -14,24 +14,47 @@ items = []
 
 
 class Item(Resource):
+
+
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+        type=float,
+        required=True,
+        help="This field cannot be left blank!"
+    )
+     
+
     @jwt_required()
     def get(self, name):
         item = next(filter(lambda  x: x['name'] == name, items), None)
         return {'item': item}, 200 if item is not None else 404
 
     def post(self, name):
-
         if next(filter(lambda  x: x['name'] == name, items), None) is not None:
             return {'message': "An item with name {} already exists.".format(name)}, 400
 
-
-        data = request.get_json()
+        data = Item.parser.parse_args()
 
         item = {'name': data['name'], 'price': data['price']}
         items.append(item)
         return item, 201
 
+    @jwt_required()
+    def delete(self, name):
+        global items
+        items = list(filter(lambda  x: x['name'] != name, items))
+        return {'message': 'Item deleted'}
 
+    def put(self, name):
+        
+        data = Item.parser.parse_args()
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        if item is None:
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
+        else:
+            item.update(data)
+        return item
 
 class ItemList(Resource):
     def get(self):
--- FlaskPractising/restfull_api/security.py
+++ FlaskPractising/restfull_api/security.py
@@ -10,10 +10,12 @@ users = [
 username_mapping = {u.username: u for u in users}
 userid_mapping = {u.id: u for u in users}
 
+
 def authenticate(username, password):
     user = username_mapping.get(username, None)
     if user and user.password == password:
         return user
+
 
 def identity(payload):
     user_id = payload['identity']
