0588683d84c431ae3fcefc471e00d1188109694e
AnastasiosMargaritis <anastasismargaritis@gmail.com>
2020-10-09 13:48:20 +0300

    Creating my first RESTFUL Flask API

---
+++ FlaskPractising/restfull_api/app.py
@@ -0,0 +1,35 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+items = []
+
+
+class Item(Resource):
+    def get(self, name):
+        for item in items:
+            if item['name'] == name:
+                return item
+        return {'item': None}, 404
+
+    def post(self, name):
+        data = request.get_json()
+
+        item = {'name': data['name'], 'price': data['price']}
+        items.append(item)
+        return item, 201
+
+
+
+class ItemList(Resource):
+    def get(self):
+        return {'items': items}
+
+
+
+api.add_resource(Item, '/item/<string:name>') # http://127.0.0.1:500/item/Anas
+api.add_resource(ItemList, '/items')
+
+app.run(port=5000, debug=True)
