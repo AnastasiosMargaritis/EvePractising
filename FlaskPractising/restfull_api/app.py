142ce0a66884b55332cfa6619b588b527011a4b0
AnastasiosMargaritis <anastasismargaritis@gmail.com>
2020-10-09 15:38:36 +0300

    Implementing JWT Authentication

--- FlaskPractising/restfull_api/app.py
+++ FlaskPractising/restfull_api/app.py
@@ -1,20 +1,30 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required 
+
+from security import authenticate, identity
 
 app = Flask(__name__)
+app.secret_key = 'anasmarg'
 api = Api(app)
+
+jwt = JWT(app, authenticate, identity)
 
 items = []
 
 
 class Item(Resource):
+    @jwt_required()
     def get(self, name):
-        for item in items:
-            if item['name'] == name:
-                return item
-        return {'item': None}, 404
+        item = next(filter(lambda  x: x['name'] == name, items), None)
+        return {'item': item}, 200 if item is not None else 404
 
     def post(self, name):
+
+        if next(filter(lambda  x: x['name'] == name, items), None) is not None:
+            return {'message': "An item with name {} already exists.".format(name)}, 400
+
+
         data = request.get_json()
 
         item = {'name': data['name'], 'price': data['price']}
