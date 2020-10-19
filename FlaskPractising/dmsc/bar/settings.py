913cb27a872bb4764c5f073336b069a24099106f
AnastasiosMargaritis <anastasismargaritis@gmail.com>
2020-10-19 10:41:29 +0300

    Applying gunicorn to bar/customer mssc

---
+++ FlaskPractising/dmsc/bar/settings.py
@@ -0,0 +1,41 @@
+X_DOMAINS ="*"
+
+MONGO_URI = "mongodb://localhost:27017/bar"
+
+RESOURCE_METHODS = ['GET', 'POST', 'DELETE']
+ITEM_METHODS = ['GET', 'PATCH', 'PUT', 'DELETE']
+
+#Domain Definition
+schema = {
+
+    'drink_type': {
+        'type': 'string',
+        'required': True,
+        'unique': True,
+    },
+
+    'quantity': {
+        'type': 'float',
+        'required': True,
+        'min': 0
+    },
+
+    'price': {
+        'type': 'float',
+        'required': True,
+    }
+}
+
+bar = {
+
+    'additional_lookup': {
+        'url': 'regex("[\w]+")',
+        'field': 'drink_type'
+    },
+
+    'schema': schema
+}
+
+DOMAIN = {
+    'bar': bar
+}
---
+++ FlaskPractising/dmsc/customer/settings.py
@@ -0,0 +1,25 @@
+X_DOMAINS ="*"
+
+MONGO_URI = "mongodb://localhost:27017/customer"
+
+RESOURCE_METHODS = ['GET', 'POST', 'DELETE']
+ITEM_METHODS = ['GET', 'PATCH', 'PUT', 'DELETE']
+
+#Domain Definition
+schema = {
+
+    'firstname': {
+        'type': 'string',
+        'required': True,
+        'unique': True,
+    },
+
+    'bill': {
+        'type': 'float',
+        'default': 0.0
+    },
+}
+
+DOMAIN = {
+    'customer': {'schema': schema},
+}
