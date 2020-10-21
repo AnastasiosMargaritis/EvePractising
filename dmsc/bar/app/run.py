ba994b6b53de0db7fda9f722b736cbe1937473b1
AnastasiosMargaritis <anastasismargaritis@gmail.com>
2020-10-21 16:01:31 +0300

    Enabling logging for all microservices.

--- dmsc/bar/app/run.py
+++ dmsc/bar/app/run.py
@@ -1,8 +1,29 @@
 from eve import Eve
 import requests
+import logging
+
+app = Eve()
+
+def log_every_get_request(resource, request, payload):
+    app.logger.info(request)
+
+def log_every_post(resource, request):
+    app.logger.info(resource)
+
+def log_every_delete(resource, request, payload):
+    app.logger.info(payload)
+
+@app.after_request
+def after(response):
+    app.logger.info(response)
+    return response
+
+app.on_post_GET += log_every_get_request
+app.on_pre_POST += log_every_post
+app.on_pre_DELETE += log_every_delete
 
 def create_app():
-    app = Eve()
+
 
     @app.route('/bar/order/<drink_type>', methods=['GET'])
     def bar_order(drink_type):
@@ -22,5 +43,23 @@ def create_app():
             print('Quantity updated, server status: ', r.status_code)
         
         return requests.get('http://localhost:8081/bar/{}'.format(drink_type)).json()
+
+    # enable logging to 'app.log' file
+    handler = logging.FileHandler('app.log')
+
+    # set a custom log format, and add request
+    # metadata to each log line
+    handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s '
+        '[in %(filename)s:%(lineno)d] -- ip: %(clientip)s, '
+        'url: %(url)s, method:%(method)s'))
+
+    # the default log level is set to WARNING, so
+    # we have to explictly set the logging level
+    # to INFO to get our custom message logged.
+    app.logger.setLevel(logging.INFO)
+
+    # append the handler to the default application logger
+    app.logger.addHandler(handler)
 
     return app
--- dmsc/customer/app/run.py
+++ dmsc/customer/app/run.py
@@ -1,11 +1,30 @@
 from eve import Eve
 import json
 import requests
+import logging
 
+app = Eve()
 
+def log_every_get_request(resource, request, payload):
+    app.logger.info(request)
+
+def log_every_post(resource, request):
+    app.logger.info(resource)
+
+def log_every_delete(resource, request, payload):
+    app.logger.info(payload)
+
+@app.after_request
+def after(response):
+    app.logger.info(response)
+    return response
+
+app.on_post_GET += log_every_get_request
+app.on_pre_POST += log_every_post
+app.on_pre_DELETE += log_every_delete
 
 def create_app():
-    app = Eve()
+    
 
     @app.route('/customer/order/<drink_type>/<customer_id>', methods=['POST'])
     def order(drink_type, customer_id):
@@ -28,5 +47,24 @@ def create_app():
 
         return requests.get('http://localhost:8082/customer/{}'.format(customer_id)).json()
     
+    # enable logging to 'app.log' file
+    handler = logging.FileHandler('app.log')
+
+    # set a custom log format, and add request
+    # metadata to each log line
+    handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s '
+        '[in %(filename)s:%(lineno)d] -- ip: %(clientip)s, '
+        'url: %(url)s, method:%(method)s'))
+
+    # the default log level is set to WARNING, so
+    # we have to explictly set the logging level
+    # to INFO to get our custom message logged.
+    app.logger.setLevel(logging.INFO)
+
+    # append the handler to the default application logger
+    app.logger.addHandler(handler)
+
+
     return app
 
