dc20bc9f05c809ba914f896488c83fc6319be3b9
AnastasiosMargaritis <anastasismargaritis@gmail.com>
2020-11-02 14:19:58 +0200

    Configuring Swagger with Gunicorn deployment.

--- SwaggerAPI/swagger/app/run.py
+++ SwaggerAPI/swagger/app/run.py
@@ -1,23 +1,25 @@
 from eve import Eve
 from eve_swagger import get_swagger_blueprint, add_documentation
 
-app = Eve()
-swagger = get_swagger_blueprint()
-app.register_blueprint(swagger)
 
-# required. See http://swagger.io/specification/#infoObject for details.
-app.config['SWAGGER_INFO'] = {
-    'title': 'My Supercool API',
-    'version': '1.0',
-    'description': 'an API description',
-    'termsOfService': 'my terms of service',
-    'schemes': ['http', 'https'],
-}
+def create_app():
+    app = Eve()
+    swagger = get_swagger_blueprint()
+    app.register_blueprint(swagger)
 
-# optional. Will use flask.request.host if missing.
-app.config['SWAGGER_HOST'] = 'https://myhost.com'
+    # required. See http://swagger.io/specification/#infoObject for details.
+    app.config['SWAGGER_INFO'] = {
+        'title': 'My Supercool API',
+        'version': '1.0',
+        'description': 'an API description',
+        'termsOfService': 'my terms of service',
+        'schemes': ['http', 'https'],
+    }
+
+    # optional. Will use flask.request.host if missing.
+    app.config['SWAGGER_HOST'] = 'https://myhost.com'    
+
+    return app
 
 
 
-if __name__ == '__main__':
-    app.run()
